7af72173491971417334e2cb037576d4d74be8d3
RomanKyrychenko <kirichenko17roman@gmail.com>
2017-01-13 23:53:26 +0200

    Initial commit

--- massiv_build.R
+++ massiv_build.R
@@ -85,8 +85,181 @@ khmelnytska_obl$id <- rep("Хмельницька область",72142)
 ternopilska_obl <- distinct(ternopilska_obl)
 ternopilska_obl$id <- rep("Тернопільська область",70102)
 
+#92528
+sevastopol <- distinct(sevastopol)
+sevastopol$id <- rep("Севастополь",68828)
+
+#88021
+kherson <- distinct(kherson)
+kherson$id <- rep("Херсон",64109)
+
+#78694
+vinnytsa <- distinct(vinnytsa)
+vinnytsa$id <- rep("Вінниця",59838)
+
+#71221
+makiivka <- distinct(makiivka)
+makiivka$id <- rep("Макіївка",52377)
+
+#68010
+poltava <- distinct(poltava)
+poltava$id <- rep("Полтава",51227)
+
+#66718
+chernigiv <- distinct(chernigiv)
+chernigiv$id <- rep("Чернігів",50681)
+
+#59372
+cherkasy_schk <- distinct(cherkasy_schk)
+cherkasy_schk$id <- rep("Черкаси",44866)
+
+#64842
+kropyvnytskii <- distinct(kropyvnytskii)
+kropyvnytskii$id <- rep("Кропивницький",48314)
+
+#58455
+khmelnytsky <- distinct(khmelnytsky)
+khmelnytsky$id <- rep("Хмельницький",44343) 
+
+#62815
+
+zhytomur_schk <- distinct(zhytomur_schk)
+zhytomur_schk$id <- rep("Житомир",46502)
+
+#59058
+i_frankivsk <- distinct(i_frankivsk)
+i_frankivsk$id <- rep("Івано-Франківськ",44839)
+
+#59285
+rivne <- distinct(rivne)
+rivne$id <- rep("Рівне",44349)
+
+#60556
+kamianske <- distinct(kamianske)
+kamianske$id <- rep("Кам'янське",42643)
+
+#59770
+gorlovka <- distinct(gorlovka)
+gorlovka$id <- rep("Горловка",42732)
+
+#56509
+sumy <- distinct(sumy)
+sumy$id <- rep("Суми",42318)
+
+#53818
+kremenchuk <- distinct(kremenchuk)
+kremenchuk$id <- rep("Кременчук",39566)
+
+#47727
+lutsk <- distinct(lutsk)
+lutsk$id <- rep("Луцьк",35737)
+
+#40974
+kramatorsk<- distinct(kramatorsk)
+kramatorsk$id <- rep("Краматорськ",29442)
+
+#37905
+melitopol<- distinct(melitopol)
+melitopol$id <- rep("Мелітополь",27186)
+
+#36543
+b_tserkva<- distinct(b_tserkva)
+b_tserkva$id <- rep("Біла Церква",26583)
+
+#331134
+uzhgorod<- distinct(uzhgorod)
+uzhgorod$id <- rep("Ужгород",25770)
+
+#32825
+nikopol<- distinct(nikopol)
+nikopol$id <- rep("Нікополь",23832)
+
+#27770
+sloviansk<- distinct(sloviansk)
+sloviansk$id <- rep("Слов'янськ",19446)
+
+#26016
+berdiansk<- distinct(berdiansk)
+berdiansk$id <- rep("Бердянськ",18882)
+
+#25000 ?
+alchevsk<- distinct(alchevsk)
+alchevsk$id <- rep("Алчевськ",19285)
+
+#23826
+pavlograd<- distinct(pavlograd)
+pavlograd$id <- rep("Павлоград",17131)
+
+#
+<- distinct()
+$id <- rep("",)
+
+#
+<- distinct()
+$id <- rep("",)
+
+#
+<- distinct()
+$id <- rep("",)
+
+#
+<- distinct()
+$id <- rep("",)
+
+#
+<- distinct()
+$id <- rep("",)
+
+#
+<- distinct()
+$id <- rep("",)
+
+#
+<- distinct()
+$id <- rep("",)
+
+#
+<- distinct()
+$id <- rep("",)
+
+#
+<- distinct()
+$id <- rep("",)
+
+#
+<- distinct()
+$id <- rep("",)
+
+#
+<- distinct()
+$id <- rep("",)
+
+#
+<- distinct()
+$id <- rep("",)
+
+#
+<- distinct()
+$id <- rep("",)
+
+#
+<- distinct()
+$id <- rep("",)
+
+#
+<- distinct()
+$id <- rep("",)
+
+#
+<- distinct()
+$id <- rep("",)
+
+#
+<- distinct()
+$id <- rep("",)
+
 library(gtools)
-all_user <- smartbind(all_user,charkivska_obl,sumy_obl,if_obl,zakar_obl,kirovograd_obl,volyn_obl,poltavska_obl,vinnytska_obl,khmelnytska_obl,ternopilska_obl)
+all_user2 <- smartbind(charkivska_obl,sumy_obl,if_obl,zakar_obl,kirovograd_obl,volyn_obl,poltavska_obl,vinnytska_obl,khmelnytska_obl,ternopilska_obl)
 ggplot(all_user, aes(dmy(bdate))) + geom_histogram()
 
 ternopil_tbl <- copy_to(sc, ternopil)
