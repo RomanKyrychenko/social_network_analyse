08d4f910ef8a13ef8191136cba409e6ae4e2e0cf
RomanKyrychenko <kirichenko17roman@gmail.com>
2017-01-17 13:11:39 +0200

    Initial commit

---
+++ googlecode.R
@@ -0,0 +1,35 @@
+library(RCurl)
+library(httr)
+library(RJSONIO)
+library(dplyr)
+library(gtools)
+
+googlecode <- function(x){
+  do.call("smartbind",lapply(x,function(y){
+      y<-sub(" ", "", y)
+      request <- paste0("https://maps.googleapis.com/maps/api/geocode/json?address=", y, "&key=AIzaSyCvE3BJBByQ4VJDzQ7nOo85-dSjTRT66rM")
+      list <- tryCatch(fromJSON(getURL(request)), error=function(e) NULL)
+      if(is.null(list)){
+        a <- data_frame(
+          `Назва`=NA,
+          `lon`=NA,
+          `lat`=NA
+        )
+      } 
+      else if(length(list$results)==0){
+        a <- data_frame(
+          `Назва`=NA,
+          `lon`=NA,
+          `lat`=NA
+        )
+      } else {
+        a <- data_frame(
+          `Назва`=list$results[[1]]$address_components[[1]]$long_name,
+          `lon`=list$results[[1]]$geometry$location[1],
+          `lat`=list$results[[1]]$geometry$location[2]
+        )
+      }
+      a
+  }
+  ))
+}
